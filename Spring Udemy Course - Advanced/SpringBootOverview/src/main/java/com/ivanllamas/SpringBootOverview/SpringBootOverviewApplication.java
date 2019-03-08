51af90a5c3135835cfee29e7ee5d661baa08576d
ivanhola <ivanholalshs@gmail.com>
2019-03-07 17:00:51 -0800

    Spring Boot Overview

---
+++ Spring Udemy Course - Advanced/SpringBootOverview/src/main/java/com/ivanllamas/SpringBootOverview/SpringBootOverviewApplication.java
@@ -0,0 +1,13 @@
+package com.ivanllamas.SpringBootOverview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootOverviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootOverviewApplication.class, args);
+	}
+
+}
