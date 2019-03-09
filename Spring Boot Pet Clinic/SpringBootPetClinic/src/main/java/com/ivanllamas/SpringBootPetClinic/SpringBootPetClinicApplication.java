2d88df1397bcebd9cf4d0da20c749712f5ad8d16
ivanhola <ivanholalshs@gmail.com>
2019-03-08 18:31:25 -0800

    Starter files

---
+++ Spring Boot Pet Clinic/SpringBootPetClinic/src/main/java/com/ivanllamas/SpringBootPetClinic/SpringBootPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.ivanllamas.SpringBootPetClinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPetClinicApplication.class, args);
+	}
+
+}
