cdd77cf1f1fdf6562df51c9adcb78b48af5e38e9
ivanhola <ivanholalshs@gmail.com>
2019-03-12 19:47:17 -0700

    New project

---
+++ Spring Udemy Course - Advanced/RecipeProject/src/main/java/com/ivanllamas/RecipeProject/controller/IndexController.java
@@ -0,0 +1,15 @@
+package com.ivanllamas.RecipeProject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"", "/", "/index"})
+    public String mainPage(){
+
+        return "index";
+    }
+    
+}
