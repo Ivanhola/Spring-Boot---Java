f3484cd640357557f96fedc6e91f4e51a8e4ebbb
ivanhola <ivanholalshs@gmail.com>
2019-03-13 21:12:30 -0700

    Updated Owner and Added PetType Service interface and impl

--- SpringBootPetClinic/pet-clinic-data/src/main/java/com/ivanllamas/entity/Owner.java
+++ SpringBootPetClinic/pet-clinic-data/src/main/java/com/ivanllamas/entity/Owner.java
@@ -9,6 +9,34 @@ public class Owner extends Person {
     //owner has a set of pets list of pets
     private Set<Pet> pets;
 
+    //Add address telephone and other variables
+    private String address;
+    private String city;
+    private String telephone;
+
+    public String getAddress() {
+        return this.address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return this.city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getTelephone() {
+        return this.telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
 
     public Set<Pet> getPets() {
         return this.pets;
---
+++ SpringBootPetClinic/pet-clinic-data/src/main/java/com/ivanllamas/services/PetTypeService.java
@@ -0,0 +1,11 @@
+package com.ivanllamas.services;
+
+import com.ivanllamas.entity.PetType;
+
+/**
+ * PetTypeService
+ */
+public interface PetTypeService extends CrudService<PetType, Long>{
+
+    
+}
