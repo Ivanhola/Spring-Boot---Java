45132ba16047c7d4c376655ce75b3367723922e1
ivanhola <ivanholalshs@gmail.com>
2019-03-13 20:57:33 -0700

    Visit Entity, Owner Set

--- SpringBootPetClinic/pet-clinic-data/src/main/java/com/ivanllamas/entity/Owner.java
+++ SpringBootPetClinic/pet-clinic-data/src/main/java/com/ivanllamas/entity/Owner.java
@@ -1,7 +1,20 @@
 
 package com.ivanllamas.entity;
 
+import java.util.Set;
+
 //pet owner, also inherits from Person
 public class Owner extends Person {
+
+    private Set<Pet> pets;
+
+
+    public Set<Pet> getPets() {
+        return this.pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 
 }
---
+++ SpringBootPetClinic/pet-clinic-data/src/main/java/com/ivanllamas/entity/Visit.java
@@ -0,0 +1,37 @@
+package com.ivanllamas.entity;
+
+import java.time.LocalDate;
+
+public class Visit extends BaseEntity{
+
+private LocalDate date;
+private String description;
+private Pet pet;
+
+
+    public LocalDate getDate() {
+        return this.date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Pet getPet() {
+        return this.pet;
+    }
+
+    public void setPet(Pet pet) {
+        this.pet = pet;
+    }
+
+    
+}
