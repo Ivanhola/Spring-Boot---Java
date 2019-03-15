215423798d16671229089e82572a17c9ca9d29f9
ivanhola <ivanholalshs@gmail.com>
2019-03-14 17:39:22 -0700

    Fixed error page

--- SpringBootPetClinic/pet-clinic-web/src/main/java/com/ivanllamas/controllers/IndexController.java
+++ SpringBootPetClinic/pet-clinic-web/src/main/java/com/ivanllamas/controllers/IndexController.java
@@ -3,6 +3,9 @@ package com.ivanllamas.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 public class IndexController {
@@ -13,5 +16,11 @@ public class IndexController {
         
         return "index";
     }
+
+    @RequestMapping("/oups")
+    public String errorPage() {
+        return "error";
+    }
+    
     
 }
