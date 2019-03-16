23229c31cdb4f4ed7b9bea06aef6009a30dafa66
ivanhola <ivanholalshs@gmail.com>
2019-03-15 19:59:48 -0700

    Category entity, and many to many relationship

---
+++ Spring Udemy Course - Advanced/RecipeProject/src/main/java/com/ivanllamas/RecipeProject/model/Category.java
@@ -0,0 +1,54 @@
+
+package com.ivanllamas.RecipeProject.model;
+
+import java.util.Set;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+@Entity
+public class Category {
+
+    /*              Variables          */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    private String description;
+    
+    //looks for a variable 'categories' in our Recipe class
+    @ManyToMany(mappedBy = "categories")
+    private Set<Recipe> recipes;
+    
+    
+    
+    /*              Getters and Setters             */
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Set<Recipe> getRecipes() {
+        return recipes;
+    }
+
+    public void setRecipes(Set<Recipe> recipes) {
+        this.recipes = recipes;
+    }
+    
+    
+}
--- Spring Udemy Course - Advanced/RecipeProject/src/main/java/com/ivanllamas/RecipeProject/model/Recipe.java
+++ Spring Udemy Course - Advanced/RecipeProject/src/main/java/com/ivanllamas/RecipeProject/model/Recipe.java
@@ -8,7 +8,10 @@ import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.Lob;
+import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
@@ -47,6 +50,15 @@ public class Recipe {
     //relationship to Note object
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
+    
+    
+    //joining tables for our recipe -> category, and the columns will be from recipe to category as 'recipe_id'
+    //and from category to recipe as 'category_id'
+    @ManyToMany
+    @JoinTable(name = "recipe_category",
+            joinColumns = @JoinColumn(name = "recipe_id"), 
+            inverseJoinColumns = @JoinColumn(name = "category_id"))
+    private Set<Category> categories;
 
     //getters and setters
 
@@ -148,6 +160,15 @@ public class Recipe {
         this.difficulty = difficulty;
     }
 
+    public Set<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Set<Category> categories) {
+        this.categories = categories;
+    }
+
+    
     
 
 }
