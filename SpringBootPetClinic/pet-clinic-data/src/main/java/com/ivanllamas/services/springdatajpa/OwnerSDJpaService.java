4d0b4610cf17e105eb3c69f8b611621b4e8094c4
ivanhola <ivanholalshs@gmail.com>
2019-03-17 15:16:33 -0700

    Spring Data Service class

---
+++ SpringBootPetClinic/pet-clinic-data/src/main/java/com/ivanllamas/services/springdatajpa/OwnerSDJpaService.java
@@ -0,0 +1,73 @@
+
+package com.ivanllamas.services.springdatajpa;
+
+import com.ivanllamas.entity.Owner;
+import com.ivanllamas.repository.OwnerRepository;
+import com.ivanllamas.repository.PetRepository;
+import com.ivanllamas.repository.PetTypeRepository;
+import com.ivanllamas.services.OwnerService;
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+@Service
+@Profile("springdatajpa")
+public class OwnerSDJpaService implements OwnerService{
+
+    private final OwnerRepository ownerRepository;
+    private final PetRepository petRepository;
+    private final PetTypeRepository petTypeRepository;
+
+    @Autowired
+    public OwnerSDJpaService(OwnerRepository ownerRepository, 
+            PetRepository petRepository, PetTypeRepository petTypeRepository) {
+        this.ownerRepository = ownerRepository;
+        this.petRepository = petRepository;
+        this.petTypeRepository = petTypeRepository;
+    }
+    
+    
+   
+    @Override
+    public Owner findByLastName(String lastName) {
+       return ownerRepository.findByLastName(lastName);
+    }
+
+    @Override
+    public Set<Owner> findAll() {
+        
+    
+        return (Set<Owner>) ownerRepository.findAll();
+    }
+
+    @Override
+    public Owner findById(Long id) {
+        Optional<Owner> ownerOptional = ownerRepository.findById(id);
+        
+        if(ownerOptional.isPresent()){
+            return ownerOptional.get();
+        }else{
+            return null;
+        }
+        
+    }
+
+    @Override
+    public Owner save(Owner object) {
+        return ownerRepository.save(object);
+    }
+
+    @Override
+    public void delete(Owner object) {
+        ownerRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        ownerRepository.deleteById(id);
+    }
+
+}
