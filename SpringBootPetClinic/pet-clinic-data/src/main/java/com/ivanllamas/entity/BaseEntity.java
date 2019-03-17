b56c569b9a797a9c386c0a761c5f8a513af44a9f
ivanhola <ivanholalshs@gmail.com>
2019-03-16 23:38:44 -0700

    Updated base entity with JPA

--- SpringBootPetClinic/pet-clinic-data/src/main/java/com/ivanllamas/entity/BaseEntity.java
+++ SpringBootPetClinic/pet-clinic-data/src/main/java/com/ivanllamas/entity/BaseEntity.java
@@ -4,11 +4,23 @@ package com.ivanllamas.entity;
 
 
 import java.io.Serializable;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 
 
-//this will be the 'base' entity that houses an ID value that our entities will implement.
+/*
+*this will be the 'base' entity that houses an ID value that our entities will implement.
+*/
+
+//MappedSuperclass tells spring data that other objects will derive from this entity (other classes extend this)
+
+@MappedSuperclass
 public class BaseEntity implements Serializable{
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
