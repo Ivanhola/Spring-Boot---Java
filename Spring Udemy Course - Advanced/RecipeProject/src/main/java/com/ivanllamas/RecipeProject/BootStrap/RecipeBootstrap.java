3e5f0e38c11b5d7302e7386b448136fdbb511f2b
ivanhola <ivanholalshs@gmail.com>
2019-03-23 21:31:38 -0700

    Added the rest of the displays

--- Spring Udemy Course - Advanced/RecipeProject/src/main/java/com/ivanllamas/RecipeProject/BootStrap/RecipeBootstrap.java
+++ Spring Udemy Course - Advanced/RecipeProject/src/main/java/com/ivanllamas/RecipeProject/BootStrap/RecipeBootstrap.java
@@ -162,6 +162,10 @@ public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEven
         guacRecipe.getCategories().add(americanCategory);
         guacRecipe.getCategories().add(mexicanCategory);
         
+        guacRecipe.setUrl("http://www.simplyrecipes.com/recipes/perfect_guacamole");
+        guacRecipe.setServings(4);
+        guacRecipe.setSource("Simply Recipes");
+        
         //add to our List variable.
         recipes.add(guacRecipe);
         
@@ -221,6 +225,10 @@ public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEven
 
         tacosRecipe.getCategories().add(americanCategory);
         tacosRecipe.getCategories().add(mexicanCategory);
+        
+        tacosRecipe.setUrl("http://www.simplyrecipes.com/recipes/spicy_grilled_chicken_tacos/");
+        tacosRecipe.setServings(4);
+        tacosRecipe.setSource("Simply recipes");
         
         
         recipes.add(tacosRecipe);
