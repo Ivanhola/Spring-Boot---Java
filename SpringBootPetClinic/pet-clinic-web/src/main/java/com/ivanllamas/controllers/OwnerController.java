b605decfdad2e389eaa8c593c6500aec9caf6190
ivanhola <ivanholalshs@gmail.com>
2019-03-27 23:45:28 -0700

    AddedOwner controller method and templates

--- SpringBootPetClinic/pet-clinic-web/src/main/java/com/ivanllamas/controllers/OwnerController.java
+++ SpringBootPetClinic/pet-clinic-web/src/main/java/com/ivanllamas/controllers/OwnerController.java
@@ -6,8 +6,11 @@ import com.ivanllamas.services.OwnerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
+@RequestMapping("/owners")
 @Controller
 public class OwnerController {
 
@@ -22,7 +25,7 @@ public class OwnerController {
 
     
 
-    @RequestMapping({"/owners","/owners/index","/owners/index.html"})
+    @RequestMapping({"","/","/index","/index.html"})
     public String ownersMain(Model model){
         //returns a set that we can iterate in our thymeleaf template
         model.addAttribute("owners", ownerService.findAll());
@@ -33,6 +36,18 @@ public class OwnerController {
     @RequestMapping("/find")
     public String findOwners(){
         return "error";
+    }
+    
+    /*GET MAPPING TO DISPLAY AN INDIVIDUAL OWNER, RETURNS OWNERDETAILS.HTML*/
+    @RequestMapping("/{ownerId}")
+    public ModelAndView showOwner(@PathVariable("ownerId") Long ownerId){
+        //the return html view
+        ModelAndView mav = new ModelAndView("owners/ownerDetails");
+        
+        //returns the object type/object to work with in the thymeleaf model
+        mav.addObject(this.ownerService.findById(ownerId));
+        
+        return mav;
     }
 }
 
