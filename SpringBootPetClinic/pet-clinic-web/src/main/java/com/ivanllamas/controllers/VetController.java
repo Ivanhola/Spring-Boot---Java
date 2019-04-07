6a01a40d3c2975b85e24d2431499cd6e13595ce3
ivanhola <ivanholalshs@gmail.com>
2019-04-07 13:08:46 -0700

    Added JSON api endpoint

--- SpringBootPetClinic/pet-clinic-web/src/main/java/com/ivanllamas/controllers/VetController.java
+++ SpringBootPetClinic/pet-clinic-web/src/main/java/com/ivanllamas/controllers/VetController.java
@@ -1,12 +1,15 @@
 
 package com.ivanllamas.controllers;
 
+import com.ivanllamas.entity.Vet;
 import com.ivanllamas.services.VetService;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class VetController {
@@ -24,6 +27,13 @@ public class VetController {
         //returns a set of vets
         model.addAttribute("vets", vetService.findAll());
         return "vets/index";
+    }
+    
+    //returns a JSON format
+    @RequestMapping("/api/vets")
+    public @ResponseBody Set<Vet> getVetsJson(){
+        
+        return vetService.findAll();
     }
     
 }
