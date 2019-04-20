938e291473d5c9fdef8019c74f21354fcf41cf8c
ivanhola <ivanholalshs@gmail.com>
2019-04-19 21:16:20 -0700

    Added Youtube Project

---
+++ YoutubeVideoViewer/src/main/java/com/ivanllamas/YoutubeVideoViewer/YoutubeVideoViewerApplication.java
@@ -0,0 +1,13 @@
+package com.ivanllamas.YoutubeVideoViewer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoutubeVideoViewerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoutubeVideoViewerApplication.class, args);
+	}
+
+}
