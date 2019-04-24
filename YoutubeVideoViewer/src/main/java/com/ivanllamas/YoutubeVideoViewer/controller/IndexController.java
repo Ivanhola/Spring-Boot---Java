3895499671027463cabe84622538e5435bb40dad
ivanhola <ivanholalshs@gmail.com>
2019-04-23 19:06:11 -0700

    Added search funcionality

--- YoutubeVideoViewer/src/main/java/com/ivanllamas/YoutubeVideoViewer/controller/IndexController.java
+++ YoutubeVideoViewer/src/main/java/com/ivanllamas/YoutubeVideoViewer/controller/IndexController.java
@@ -2,11 +2,15 @@
 package com.ivanllamas.YoutubeVideoViewer.controller;
 
 import com.google.api.client.googleapis.json.GoogleJsonResponseException;
+import com.ivanllamas.YoutubeVideoViewer.entity.ItemToSearch;
 import com.ivanllamas.YoutubeVideoViewer.service.YoutubeService;
 import java.io.IOException;
 import java.security.GeneralSecurityException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -20,10 +24,20 @@ public class IndexController {
     }
     
     @RequestMapping("/")
-    public String home() throws GeneralSecurityException, IOException, GoogleJsonResponseException{
-        youtubeService.searchVideos("Helloworld");
+    public String home(Model model){
         
+        ItemToSearch search = new ItemToSearch();
+        
+        model.addAttribute("search", search);
         return "home";
+    }
+    
+    @PostMapping("/search")
+    public String searchProcessed(@ModelAttribute("search") ItemToSearch searched, Model model)
+    throws GeneralSecurityException, IOException, GoogleJsonResponseException{
+        
+        model.addAttribute("list",youtubeService.searchVideos(searched.getSearchItem()));
+        return "videoSearched";
     }
     
     
---
+++ YoutubeVideoViewer/src/main/java/com/ivanllamas/YoutubeVideoViewer/entity/ItemToSearch.java
@@ -0,0 +1,25 @@
+
+package com.ivanllamas.YoutubeVideoViewer.entity;
+
+
+public class ItemToSearch {
+    private String searchItem;
+
+    public ItemToSearch() {
+    }
+
+    public ItemToSearch(String searchItem) {
+        this.searchItem = searchItem;
+    }
+
+    public String getSearchItem() {
+        return searchItem;
+    }
+
+    public void setSearchItem(String searchItem) {
+        this.searchItem = searchItem;
+    }
+    
+    
+    
+}
